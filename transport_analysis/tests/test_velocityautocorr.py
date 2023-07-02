1b5e81bda74ded9df2e6753d21d3a5459a159321
Xu Hong Chen <110699064+xhgchen@users.noreply.github.com>
2023-07-02 13:24:23 -0700

    Update import in `test_velocityautocorr.py`

--- transport_analysis/tests/test_velocityautocorr.py
+++ transport_analysis/tests/test_velocityautocorr.py
@@ -1,7 +1,7 @@
 import pytest
 from numpy.testing import assert_almost_equal
 
-from transport_analysis.analysis.velocityautocorr import (
+from transport_analysis.velocityautocorr import (
     VelocityAutocorr as VACF,
 )
 import MDAnalysis as mda
