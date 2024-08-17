bdb3e2f2a784ee290b1c8eb947bc9e52c2641972
orionarcher <ocohen@radical-ai.com>
2024-08-17 13:40:17 -0400

    New test files for a ethylene carbonate system. Meant to test viscosity.

--- transport_analysis/data/files.py
+++ transport_analysis/data/files.py
@@ -10,8 +10,13 @@ Use as ::
 
 __all__ = [
     "MDANALYSIS_LOGO",  # example file of MDAnalysis logo
+    "ec_traj_trr",  # ethylene carbonate trajectory in h5md format
+    "ec_top",  # ethylene carbonate topology
 ]
 
 from pkg_resources import resource_filename
 
 MDANALYSIS_LOGO = resource_filename(__name__, "mda.txt")
+
+ec_traj_trr = resource_filename(__name__, "ethylene_carbonate/trajectory.trr")
+ec_top = resource_filename(__name__, "ethylene_carbonate/topology.pdb")
