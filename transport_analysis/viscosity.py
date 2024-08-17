Fix viscosity implementation, add automatic calculation of true viscosity, add plot of viscosity function for checking location of window.

transport_analysis/viscosity.py
@@ -68,6 +68,7 @@ class ViscosityHelfand(AnalysisBase):
         atomgroup: "AtomGroup",
         temp_avg: float = 300.0,
         dim_type: str = "xyz",
+        linear_fit_window: tuple[int, int] = None,
         **kwargs,
     ) -> None:
         # the below line must be kept to initialize the AnalysisBase class!
@@ -86,6 +87,7 @@ class ViscosityHelfand(AnalysisBase):
         # args
         self.temp_avg = temp_avg
         self.dim_type = dim_type.lower()
+        self.linear_fit_window = linear_fit_window
         self._dim, self.dim_fac = self._parse_dim_type(self.dim_type)
 
         # local
@@ -203,7 +205,7 @@ class ViscosityHelfand(AnalysisBase):
             )
 
             # square and sum each x(, y, z) diff per particle
-            sq_diff = np.square(diff).sum(axis=-1)
+            sq_diff = np.square(diff).mean(axis=-1)
 
             # average over # frames
             # update viscosity by particle array
@@ -215,3 +217,32 @@ class ViscosityHelfand(AnalysisBase):
         )
         # average over # particles and update results array
         self.results.timeseries = self.results.visc_by_particle.mean(axis=1)
+
+        if self.linear_fit_window is not None:
+            fit_start, fit_end = self.linear_fit_window[0], self.linear_fit_window[1]
+            linear_fit = np.polyfit(
+                lagtimes[fit_start:fit_end],
+                self.results.timeseries[fit_start:fit_end],
+                1
+            )
+            self.results.viscosity = linear_fit[0]
+
+    def plot_viscosity_function(self):
+        """
+        Plot the viscosity function as a function of lag-time.
+        """
+        import matplotlib.pyplot as plt
+
+        lagtimes = np.arange(0, self.n_frames)
+        plt.plot(lagtimes, self.results.timeseries, label="Viscosity Function")
+
+        if self.linear_fit_window is not None:
+            fit_start, fit_end = self.linear_fit_window[0], self.linear_fit_window[1]
+            plt.axvline(fit_start, color='red', linestyle='--', label='Fit Start')
+            plt.axvline(fit_end, color='blue', linestyle='--', label='Fit End')
+
+        plt.xlabel("Lag-time")
+        plt.ylabel("Viscosity Function")
+        plt.title("Viscosity Function vs Lag-time")
+        plt.legend()
+        plt.show()